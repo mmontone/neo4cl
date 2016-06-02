ee49f8c571fa9f7b21fb7a6ea2151328c4d974a9
James Fleming <james@electronic-quill.net>
2016-06-02 10:06:48 +0200

    Update error-handling in neo4j-transaction for 3.0
    neo4j-transaction now recognises the four classifications of status code
    that Neo4J can return, and signals conditions/errors accordingly.

--- src/neo4cl.lisp
+++ src/neo4cl.lisp
@@ -51,15 +51,15 @@
 
 (defun neo4j-cypher-get-request (endpoint uri)
   "Send a GET request to the legacy Cypher API, and return the parsed result.
-DEPRECATED: use neo4j-transaction instead, unless you have a specific reason not to."
+   DEPRECATED: use neo4j-transaction instead, unless you have a specific reason not to."
   (multiple-value-bind (content code headers uri stream ignore reason)
-      (drakma:http-request
-       (concatenate 'string (base-url endpoint) uri)
-       :accept "application/json; charset=UTF-8"
-       :content-type "application/json"
-       :additional-headers `(("authorization"
-                              ,(concatenate 'string "Basic "
-                                            (cl-base64:string-to-base64-string
+    (drakma:http-request
+      (concatenate 'string (base-url endpoint) uri)
+      :accept "application/json; charset=UTF-8"
+      :content-type "application/json"
+      :additional-headers `(("authorization"
+                             ,(concatenate 'string "Basic "
+                                           (cl-base64:string-to-base64-string
                                              (format nil "~A:~A"
                                                      (dbuser endpoint)
                                                      (dbpasswd endpoint)))))))
@@ -73,9 +73,9 @@ DEPRECATED: use neo4j-transaction instead, unless you have a specific reason not
 
 (defun get-user-status (server)
   "Authenticate to the Neo4J server and confirm the status of the database user.
-  200/OK means the credentials are good.
-  401/Unauthorized means they're not.
-  The presence of (:PASSWORD--CHANGE--REQUIRED . T) in the body of the reply, well, you get the hint."
+   200/OK means the credentials are good.
+   401/Unauthorized means they're not.
+   The presence of (:PASSWORD--CHANGE--REQUIRED . T) in the body of the reply, well, you get the hint."
   (neo4j-cypher-get-request server (concatenate 'string "/user/" (dbuser server))))
 
 (defun neo4j-cypher-post-request (endpoint content &optional uri)
@@ -103,7 +103,7 @@ DEPRECATED: use neo4j-transaction instead, unless you have a specific reason not
 (defun change-password (server new-password)
   "Change a Neo4J user's password.
    If the db operation is successful, update the server object so that this takes immediate effect within this server.
-   NB: remember to update whatever you're using to init"
+   NB: remember to update whatever you're using to initialise your db connection."
   (multiple-value-bind (body numeric verbal headers)
     (neo4j-cypher-post-request server
                                `((:password . ,new-password))
@@ -117,25 +117,50 @@ DEPRECATED: use neo4j-transaction instead, unless you have a specific reason not
 
 ;;; The modern interface
 
-(define-condition neo4j-transaction-error (error)
+;; Classes of Neo4J status codes
+;; Based on the documentation at http://neo4j.com/docs/developer-manual/current/#status-codes
+
+;; The Client sent a bad request - changing the request might yield a successful outcome.
+;; Effect on transaction: rollback
+(define-condition client-error (error)
   ((code :initarg :code :reader code)
+   (category :initarg :category :reader category)
+   (title :initarg :title :reader title)
    (message :initarg :message :reader message)))
 
-(define-condition neo4j-invalid-format-error (neo4j-transaction-error)
+;; There are notifications about the request sent by the client.
+;; Effect on transaction: none
+(define-condition client-notification ()
   ((code :initarg :code :reader code)
+   (category :initarg :category :reader category)
+   (title :initarg :title :reader title)
    (message :initarg :message :reader message)))
 
-(define-condition neo4j-integrity-error (neo4j-transaction-error)
+;; The database cannot service the request right now, retrying later might yield a successful outcome.
+;; Effect on transaction: rollback
+(define-condition transient-error (error)
   ((code :initarg :code :reader code)
+   (category :initarg :category :reader category)
+   (title :initarg :title :reader title)
    (message :initarg :message :reader message)))
+
+;; The database failed to service the request.
+;; Effect on transaction: rollback
+(define-condition database-error (error)
+  ((code :initarg :code :reader code)
+   (category :initarg :category :reader category)
+   (title :initarg :title :reader title)
+   (message :initarg :message :reader message)))
+
 
 (defun neo4j-transaction (endpoint statements)
   "Execute one or more Cypher statements, wrapped in a transaction.
-  For now, we're simply issuing all statements in a batch and then committing, instead of building it up over several HTTP requests.
-  Sample input:
-  '((:statements
-      ((:statement . \"MATCH (n:User {properties} ) RETURN n\")
-       (:parameters (:properties (:name . \"bob\"))))))"
+   For now, we're simply issuing all statements in a batch and then committing, instead of building it up over several HTTP requests.
+   Sample input:
+   '((:statements
+       ((:statement . \"MATCH (n:User {properties} ) RETURN n\")
+        (:parameters (:properties (:name . \"bob\"))))))
+   If an error is detected, it will be signalled with whatever information was provided by Neo4J"
   (multiple-value-bind (reply-content code headers uri stream ignore reason)
     (drakma:http-request (concatenate 'string (base-url endpoint) "/db/data/transaction/commit")
                          :method :post
@@ -159,24 +184,28 @@ DEPRECATED: use neo4j-transaction instead, unless you have a specific reason not
       ;; Process the response we got, returning either the content or an error
       (let* ((response (decode-neo4j-json reply-content))
              (errors (second (second response))))
-        (if
-          ;; If an error was returned, throw it
-          errors
-          (let ((error-code (cdr (assoc :code errors))))
-            (error
-              (cond
-                ;; Schema violation.
-                ;; Usually because we attempted to create a user that already exists
-                ((equal error-code "Neo.ClientError.Schema.ConstraintViolation")
-                 'neo4j-integrity-error)
-                ;; Usually means we goofed when generating the JSON for the query
-                ((equal error-code "Neo.ClientError.Request.InvalidFormat")
-                 'neo4j-invalid-format-error)
-                ;; Anything else.
-                (t
-                  'neo4j-transaction-error))
-              :code (cdr (assoc :code errors))
-              :message (cdr (assoc :message errors))))
+        ;; If an error was returned, throw it
+        (if errors
+          (let* ((error-code (cdr (assoc :code errors)))
+                 (error-components (cl-ppcre:split "\\." error-code))
+                 (classification (second error-components))
+                 (category (third error-components))
+                 (title (fourth error-components)))
+            (cond
+              ;; The Client sent a bad request - changing the request might yield a successful outcome.
+              ((equal classification "ClientError")
+               (error 'client-error :category category :title title :message (cdr (assoc :message errors))))
+              ;; There are notifications about the request sent by the client.
+              ((equal classification "ClientNotification")
+               (warn 'client-notification :category category :title title :message (cdr (assoc :message errors))))
+              ;; The database cannot service the request right now, retrying later might yield a successful outcome.
+              ((equal classification "TransientError")
+               (error 'transient-error :category category :title title :message (cdr (assoc :message errors))))
+              ;; The database failed to service the request.
+              ((equal classification "DatabaseError")
+               (error 'database-error :category category :title title :message (cdr (assoc :message errors))))
+              ;; Anything else.
+              (t (error "Unknown error occurred"))))
           ;; If everything's OK, return the values we received along with the status codes
           (values response code reason)))
       ;;; Restart cases start here
